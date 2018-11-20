b2759f40525bcd00704dc50e62ae55bd34ce88b0
Jonathan Paetzold <jcp.lucky@gmail.com>
2018-11-20 17:27:22 -0500

    adding the project and the barebones click to place

---
+++ Assets/Scripts/ItemPlacer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlacer : MonoBehaviour
+{
+    private BasicGrid grid;
+
+    public GameObject flashlight;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<BasicGrid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject item = Instantiate(flashlight);
+        item.transform.position = finalPosition;
+        //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+
+    }
+}
